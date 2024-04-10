59f65a18e2bb3fd39407288b2ad1b814eec79162
Marvin Cámbara <marvin75205@gmail.com>
2024-04-10 10:42:23 -0600

    Agregar archivos de proyecto.

---
+++ Carrito/Carro/Carro.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carrito.Carro
+{
+    internal class Carro
+    {
+        public bool bocina { get; set; }
+        public bool closh {  get; set; }
+        public string Marca { get; }
+        private int desa {  get; }
+        public bool FrenoMano { get; set;  }
+        public bool luces {  get; set; }
+        public int palanca { get; set; }
+        public int Modeolo { get; }
+        public string Color { get; set; }
+        public string owner { get; set; }
+        public bool Encendido { get; set; }
+        public int velocidad_actual { get; set; }
+        private int MAXVELOCIDAD { get;}
+        private int Revolucion { get; }
+        private int capafreno { get; }
+        public Carro(string m, int model, string col, string dueño)
+        {
+            capafreno = 10;
+            desa = 5;
+            Marca = m;
+            Modeolo = model;
+            Color = col;
+            closh = false;
+            palanca = 0;
+            bocina = false;
+            owner = dueño;
+            luces = false;
+            Revolucion = 5;
+            FrenoMano = true;
+            Encendido = false;
+            velocidad_actual = 0;
+            MAXVELOCIDAD = 250;
+        }
+        public void Encender()
+        {
+            if (!Encendido)
+            {
+                Encendido = true;
+            }
+            else
+            {
+                {
+                    if (palanca == 0 && FrenoMano)
+                    {
+                        Encendido = false;
+                    }
+                }
+            }
+        }
+        public void Chloshar()
+        {
+            if(!closh)
+            {
+                closh = true;
+            }
+            else
+            {
+                closh = false;
+            }
+        }
+        public void Subirfreno()
+        {
+           if(!FrenoMano && velocidad_actual <= desa)
+            {
+                FrenoMano = true;
+                velocidad_actual = 0;         
+            }
+            else
+            {
+                FrenoMano = false;
+            }
+        }
+
+        public void Encenderluces()
+        {
+            if (luces)
+            {
+                luces = false;
+            }
+            else
+            {
+                luces = true;
+            }
+        }
+        public int Acelerar()
+        {
+            if (Encendido && velocidad_actual < MAXVELOCIDAD && palanca != 0 && !closh)
+            {
+                if (FrenoMano && velocidad_actual < MAXVELOCIDAD)
+                {
+                    velocidad_actual += 5;
+                    return velocidad_actual;
+                }
+                else
+                {
+                    velocidad_actual += (palanca * Revolucion) ;
+                    if (velocidad_actual > MAXVELOCIDAD)
+                    {
+                        velocidad_actual = MAXVELOCIDAD;
+                        return velocidad_actual;
+                    }
+                    else
+                    {
+                        return velocidad_actual;
+                    }
+                }
+            }
+            else if (velocidad_actual > MAXVELOCIDAD)
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                return velocidad_actual;
+            }
+            return velocidad_actual;
+        }
+        public void Frenos()
+        {
+            if (velocidad_actual > desa)
+            {
+                velocidad_actual -= capafreno;
+            }
+        }
+        public int salir(int op)
+        {
+            if(!Encendido && FrenoMano && velocidad_actual == 0 && palanca == 0 && !luces)
+            {
+                return 0;
+            }
+            return 9;
+        }
+        public void desacelr()
+        {
+            if (velocidad_actual > desa)
+            {
+                velocidad_actual -= desa;
+            }
+        }
+        public int cambio(int cambio)
+        {
+            if (Encendido && closh)
+            {
+                if (cambio == 6 && palanca < 6 && velocidad_actual >= (palanca) * Revolucion)
+                {
+                    palanca++;
+                    return palanca;
+                }
+                else if (cambio == 7 && palanca > 0)
+                {
+                    palanca--;
+                    return palanca;
+                }
+            }
+            return palanca;
+        }
+        public void bocinar()
+        {
+            if(Encendido)
+            {
+                if(!bocina)
+                {
+                    Console.Beep();
+                    bocina = true;
+                }
+                else
+                {
+                    Console.Beep();
+                    bocina = false;
+                }
+            }
+        }
+    }
+}
---
+++ Carrito/Program.cs
@@ -0,0 +1,221 @@
+﻿using Carrito.Carro;
+Carro miCarro = new Carro("Toyota", 2024, "Rojo", "Juan");
+int op = 0, op2 = 0;
+Console.WriteLine("Bienvenido ¿Cómo se llama?\n");
+Console.Write("> ");
+miCarro.owner = Console.ReadLine();
+Console.WriteLine("\n¿De que color desea su auto?\n");
+Console.Write("> ");
+miCarro.Color = Console.ReadLine();
+Console.Clear();
+do
+{
+    try
+    {
+        Console.Clear();
+        Console.WriteLine($"Bienvenid@ {miCarro.owner} ¿Qué desa hacer hoy en su {miCarro.Marca} modelo {miCarro.Modeolo} de color {miCarro.Color}?\n");
+        Console.WriteLine("1) Manejar");
+        Console.WriteLine("0) No hacer nada\n");
+        Console.Write("> ");
+        op = Convert.ToInt32(Console.ReadLine());
+        if (op != 0)
+        {
+           
+                switch (op)
+                {
+                    case 1:
+                        do
+                        {
+                        try
+                        {
+                            Console.Clear();
+                            if (miCarro.Encendido)
+                            {
+                                Console.Write("Estado del auto:");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write(" Encendido");
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.Write("Estado del auto:");
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write(" Apagado");
+                                Console.ResetColor();
+                            }
+                            if (miCarro.FrenoMano)
+                            {
+                                Console.Write("\nEstado del freno de mano:");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write(" Arriba");
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.Write("\nEstado del freno de mano:");
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write(" Abajo");
+                                Console.ResetColor();
+                            }
+                            if (miCarro.luces)
+                            {
+                                Console.Write("\nEstado de las luces:");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write(" Encendidas");
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.Write("\nEstado de las luces:");
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write(" Apagadas");
+                                Console.ResetColor();
+                            }
+                            if (miCarro.velocidad_actual == 0)
+                            {
+                                Console.Write("\nVelocidad actual:");
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write($" {miCarro.velocidad_actual} Km/h");
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.Write("\nVelocidad actual:");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write($" {miCarro.velocidad_actual} Km/h");
+                                Console.ResetColor();
+                            }
+                            if (miCarro.closh == false)
+                            {
+                                Console.Write("\nClosh:");
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write($" Suelto");
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.Write("\nClosh:");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write($" Presionado");
+                                Console.ResetColor();
+                            }
+                            if (miCarro.palanca == 0)
+                            {
+                                Console.Write("\nVelocidades (Cambios):");
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write($" {miCarro.palanca}");
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.Write("\nVelocidades (Cambios):");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write($" {miCarro.palanca}");
+                                Console.ResetColor();
+                            }
+                            Console.WriteLine("\n\n");
+                            Console.WriteLine("1) Enceder/Apagar el auto");
+                            Console.WriteLine("2) Subir/Bajar freno de mano");
+                            Console.WriteLine("3) Encender/Apagar luces");
+                            Console.WriteLine("4) Acelerar");
+                            Console.WriteLine("5) Frenar");
+                            Console.WriteLine("6) Aumentar cambio");
+                            Console.WriteLine("7) Disminuir cambio");
+                            Console.WriteLine("8) Desacelerar");
+                            Console.WriteLine("9) Bocinar");
+                            Console.WriteLine("10) Closh");
+                            Console.WriteLine("0) Salir del auto\n");
+                            Console.Write("> ");
+                            op2 = Convert.ToInt32(Console.ReadLine());
+                            switch (op2)
+                            {
+                                case 0:
+                                    op2 = miCarro.salir(op2);
+                                    break;
+                                case 1:
+                                    miCarro.Encender();
+                                    break;
+                                case 2:
+                                    miCarro.Subirfreno();
+                                    break;
+                                case 3:
+                                    miCarro.Encenderluces();
+                                    break;
+                                case 4:
+                                    for (int i = 0; i < 3; i++)
+                                    {
+                                        Console.WriteLine($"\n{miCarro.Acelerar()} Km/h");
+                                    }
+                                    Console.ReadKey();
+                                    break;
+                                case 5:
+                                    for (int i = 0; i < 3; i++)
+                                    {
+                                        miCarro.Frenos();
+                                        Console.WriteLine($"\n{miCarro.velocidad_actual} Km/h");
+                                    }
+                                    Console.ReadKey();
+                                    break;
+                                case 6:
+                                    miCarro.cambio(op2);
+                                    break;
+                                case 7:
+                                    miCarro.cambio(op2);
+                                    break;
+                                case 8:
+                                    for (int i = 0; i < 3; i++)
+                                    {
+                                        miCarro.desacelr();
+                                        Console.WriteLine($"\n{miCarro.velocidad_actual} Km/h");
+                                    }
+                                    Console.ReadKey();
+                                    break;
+                                case 9:
+                                    miCarro.bocinar();
+                                    break;
+                                case 10:
+                                    miCarro.Chloshar();
+                                    break;
+                                default:
+                                    Console.Clear();
+                                    Console.WriteLine("La opción ingresada no es valida\n");
+                                    Console.WriteLine("Presione \"ENTER\" para continuar\n");
+                                    Console.Write("> ");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
+                            }
+                        }catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("El caracter ingresado no es valido\n");
+                Console.WriteLine("Presione \"ENTER\" para continuar\n");
+                Console.Write("> ");
+                Console.ReadKey();
+                Console.Clear();
+                op2 = 1;
+            }
+        } while (op2 != 0);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("La opción ingresada no es valida\n");
+                        Console.WriteLine("Presione \"ENTER\" para continuar\n");
+                        Console.Write("> ");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+        }
+    }catch(Exception)
+    {
+        Console.Clear();
+        Console.WriteLine("El caracter ingresado no es valido\n");
+        Console.WriteLine("Presione \"ENTER\" para continuar\n");
+        Console.Write("> ");
+        Console.ReadKey();
+        Console.Clear();
+        op = 1;
+    }
+} while(op != 0);
+Console.WriteLine($"\nQue tengas un buen día {miCarro.owner}");
